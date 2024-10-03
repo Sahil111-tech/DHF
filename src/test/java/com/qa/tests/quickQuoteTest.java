32c34ad9518c7925f011fcc3949a26ef5b5a7d58
Sahil Sharma <Sahil.Sharma@avant.org.au>
2024-10-03 08:10:45 +0530

    Initial commit of project

--- src/test/java/com/qa/tests/quickQuoteTest.java
+++ src/test/java/com/qa/tests/quickQuoteTest.java
@@ -19,7 +19,7 @@ public class quickQuoteTest extends BaseTest {
 		quote = new quickQuote();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		
-		// Step 1: Click on "Get Quote" button
+		// Step 1: Click on "Get Quote" btn
 		// Verify the visibility of the "Get Quote" button
         Assert.assertTrue(quote.isGetQuoteButtonVisible(), "Get Quote button is not visible.");
         quote.clickGetQuote();
