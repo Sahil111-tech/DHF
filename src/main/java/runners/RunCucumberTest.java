ecbc39f5ee721934bb0f230a43b7a32738c691e6
Sahil.Sharma <Sahil.Sharma@CPC-Sahil-BLBHA.avant.local>
2024-10-05 08:47:15 +0530

    Messages updated

--- src/main/java/runners/RunCucumberTest.java
+++ src/main/java/runners/RunCucumberTest.java
@@ -4,9 +4,9 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-	    features = "src/test/resources/Features", // Path to your feature files
-	    glue = {"StepDefinitions"}, // Package where your step definitions are located
-	    plugin = {"pretty", "html:target/cucumber-reports.html"} // Reports
+	    features = "src/test/resources/Features", // Path to our feature files
+	    glue = {"StepDefinitions"}, // Package where our step definitions are located
+	    plugin = {"pretty", "html:target/cucumber-reports.html"} // Our Reports
 	)
 public class RunCucumberTest extends AbstractTestNGCucumberTests {
 
