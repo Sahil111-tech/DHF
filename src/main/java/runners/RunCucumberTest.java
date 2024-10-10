9f38b58c97f00f87ba5b98a2cff1b05e5a2512d5
Sahil.Sharma <Sahil.Sharma@CPC-Sahil-BLBHA.avant.local>
2024-10-10 08:20:45 +0530

    Updated few comments

--- src/main/java/runners/RunCucumberTest.java
+++ src/main/java/runners/RunCucumberTest.java
@@ -10,7 +10,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/resources/Features", glue = { "StepDefinitions" }, plugin = { "pretty",
 		"html:reports/TestReports.html",
-		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" }, // To integrates Extent Reports to generate detailed reports.
+		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" }, // This is the plugins in cucumber to integrates Extent Reports to generate detailed reports.
 dryRun = false, 
 monochrome = true,
 publish=true
